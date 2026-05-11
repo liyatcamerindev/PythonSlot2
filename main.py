2d2e800c6954cb61397ec06904e80902f8d849aa
liyatcamerin_dev <liyatcamerin@gmail.com>
2026-05-11 11:50:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import math_operations
+from math_operations import add
+import math_operations as mt 
+
+print(math_operations.add(10,3))
+print(math_operations.sub(10,3))
+print(add(5,3))
+print(mt.add(4,8))
---
+++ math_operations.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return a+b;
+
+def sub(a,b):
+    return a-b;
+
+
+
+
+
+if __name__ == "__main__":
+    print(add(2,3))
